12aa2a1bfa76f9dd4e67d72bc4b6074d09130c82
alshchetinin <alexsander.qw@gmail.com>
2020-06-05 11:59:02 +0300

    add main page block

--- index.php
+++ index.php
@@ -10,7 +10,8 @@
       <?php foreach( $slider as $post): // Переменная должна быть названа обязательно $post (IMPORTANT) ?>
       <?php setup_postdata($post); ?>
       <div class="slider_item grid" style="background-image: url()">
-         <img class="slider_item_img" data-flickity-lazyload="<?php the_field('img_slider'); ?>" />
+         <img class="slider_item_img" data-flickity-lazyload="<?php the_field('img_slider'); ?>"
+            alt="Портфолио http://lovemarketing.ru/" />
          <div class="slider_item_wrapper">
             <a href="<?php the_permalink(); ?>" class=""></a>
             <h2 class="title">
@@ -64,8 +65,19 @@
 
 </section>
 <section class="main grid">
-   <div class="section_img">
+   <!-- <div class="section_img">
       <img class="img-responsive" src="<?php echo get_template_directory_uri() ?>/img/big_logo.svg" alt="">
+   </div> -->
+   <div class="section-logo-partner">
+      <?php while( have_rows('logos', 'option') ): the_row(); 
+
+				// переменные
+				$logo = get_sub_field('logo');
+				?>
+
+      <img src="<?php echo $logo['url']; ?>" alt="<?php echo $logo['alt'] ?>" class="img-responsive">
+
+      <?php endwhile; ?>
    </div>
    <div class="section_text">
       <h2>Ваш marketplace в&nbsp;маркетинге</h2>
@@ -75,5 +87,56 @@
    </div>
 </section>
 
+
+<?php 
+$link = get_field('pdf-presentation-link', 'option');
+if( !empty( $link ) ): ?>
+<section id="main-project-link">
+   <div class="grid main-project-link">
+      <div class="favorite-works-bg">
+         <img class="img-responsive" src="<?php echo get_template_directory_uri() ?>/img/logo-works.svg" alt="logo">
+      </div>
+      <div class="favorite-works">
+         <a href="/portfolio"></a>
+         <div>
+            <h3 class="title">Избранные<br>работы</h3>
+            <img src="<?php echo get_template_directory_uri() ?>/img/arrow_slider.svg" alt="arrow">
+         </div>
+
+         <div class="favorite-works-bg favorite-works-bg-mobile">
+            <img class="img-responsive" src="<?php echo get_template_directory_uri() ?>/img/logo-works.svg" alt="logo">
+         </div>
+      </div>
+      <div class="all-works-bg">
+         <img class="img-responsive" src="<?php echo get_template_directory_uri() ?>/img/pdf.svg" alt="logo">
+      </div>
+      <div class="all-works">
+         <div>
+            <h3 class="title">Все<br>проекты </h3>
+            <img src="<?php echo get_template_directory_uri() ?>/img/arrow_slider.svg" alt="arrow">
+         </div>
+         <a href="<?php echo $link?>"></a>
+         <div class="all-works-bg all-works-bg-mobile">
+            <img class="img-responsive" src="<?php echo get_template_directory_uri() ?>/img/pdf.svg" alt="logo">
+         </div>
+      </div>
+   </div>
+</section>
+<?php endif; ?>
+
+
+
+<seciton id="main-form">
+   <div class="grid main-form">
+      <div class="main-form__text">
+         <h3 class="title">Получите бесплатную <br>консультацию </h3>
+         <p>Заполните поля справа, и мы с вами обязательно свяжемся</p>
+      </div>
+      <div class="main-form__contact">
+         <?php echo do_shortcode('[contact-form-7 id="716" title="Получите бесплатную консультацию_copy"]'); ?>
+      </div>
+      <div class="line"></div>
+   </div>
+</seciton>
 
 <?php get_footer(); ?>
